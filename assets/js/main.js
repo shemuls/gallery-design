9a7a5f464e07ab0ec266c40c276734829d3b2e69
shemuls <61226366+shemuls@users.noreply.github.com>
2020-04-17 21:16:02 +0600

    Gallery Design for WP plugin

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function( $ ) {
+
+
+
+
+    $(".my-gellary .my-gl-box a").prettyPhoto({
+        theme       : 'dark_rounded',
+    });
+
+    $(".my-gellary").mixItUp();
+        
+        
+
+
+
+
+
+    });
